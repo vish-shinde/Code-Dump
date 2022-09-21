96c5108c3e565a22711cd38d2aecce58814a90c8
Vishal Shinde <vs.vishalshinde@outlook.com>
2022-09-21 08:16:26 +0530

    React Files

---
+++ sareen_routers/testreact/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {BrowserRouter as Router, Switch,Route, Link} from 'react-router-dom';
+
+export default function App() {
+  return (
+    <Router>
+   
+      {/*<nav>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/contact'>Contact</Link></li>
+            </ul>
+            </nav> */}
+        <Route path='/about' component={About} />
+       
+    </Router>
+    
+  );
+}
+function Home() {
+  return <h2>Home</h2>
+}
+
+function About() {
+  return <h2>About</h2>
+}
+function Contact() {
+  return <h2>Contact</h2>
+}
